7d3b3db1950234124f34ad4638b2f465b646a916
Pratika NG <pratika.05ng@gmail.com>
2025-10-29 16:58:00 +0530

    product list section prep and search params

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,15 @@
+import ProductList from "@/components/ProductList"
+import Image from "next/image"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      {/* aspect-3/1 means height will be 3 times smaller that the width */}
+      <div className="relative aspect-3/1 mb-12">
+         <Image src="/featured.png" alt="Featured Product" fill />
+      </div>
+      <ProductList/>
+    </div>
   )
 }
 
